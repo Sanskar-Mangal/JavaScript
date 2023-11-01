4d0d76f8db10436e77d5445e8d11ea5c005f4c34
Sanskar78 <88654894+Sanskar78@users.noreply.github.com>
2023-11-01 08:23:35 +0000

    container config done

---
+++ JavaScript/conversionOperation.js
@@ -0,0 +1,30 @@
+let value=3
+let negvalue=-value
+
+console.log(negvalue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+
+//String + String possible
+
+
+//IMPORTANT
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 +2);
+console.log(1 +2 + "2");
+
+//tricky code
+
+console.log(true);
+console.log(+true);
+//console.log(true+); this is error code
+console.log(+""); //output is 0
+
+
