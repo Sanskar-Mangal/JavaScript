9463adf1f5f52024e102ffc8c341bf00d6c7155e
Sanskar78 <88654894+Sanskar78@users.noreply.github.com>
2023-11-01 07:41:58 +0000

    container config done

---
+++ JavaScript/datatype.js
@@ -0,0 +1,28 @@
+"use strict";  //treat all JS code as newer version
+
+//alert(3+3) we are using node js , not browser
+
+let name="Sanskar"
+let age=22
+let isLoggedIn=true
+let state=null  //null means empty
+
+//number =>2 to power 53
+//bigint
+//string=""
+//boolean= true,false
+//null= standalone value 
+//undefined=value not assign
+//symbol => unique ness (USING REACT JS)
+
+
+//object
+
+console.log(typeof "Sanskar");  //type btya tha hai
+console.log(typeof age);        //value type btya tha hai
+
+
+//null type is object (null ek object hai)
+
+console.log(typeof null);  //output is object 
+console.log(typeof undefined);
