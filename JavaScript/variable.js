53526ede1aa68004fe81de16d54a9bc016cd0ea0
Sanskar78 <88654894+Sanskar78@users.noreply.github.com>
2023-11-01 07:23:59 +0000

    container config done

---
+++ JavaScript/variable.js
@@ -0,0 +1,19 @@
+const accountid="432154"
+let accountemail="demo@google.com"
+var accountpassword="12345"
+accountcity="BLR"
+let accountstate;
+
+//accountid="458765"
+//console.log(accountid);
+//Value not change const 
+
+/*
+perfer not be use var because of issues in block scope anf functional scope  
+*/
+
+accountemail="sanskar@google.com"
+accountpassword="32145"
+accountcity="Gwalior"
+
+console.table([accountemail,accountpassword,accountcity,accountstate])
